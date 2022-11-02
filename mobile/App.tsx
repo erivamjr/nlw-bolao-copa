132cc81bc5de9a890da5822137cfa6fe47706162
José Erivam <erivamdev@gmail.com>
2022-11-02 18:39:11 -0300

    :sparkles: feat:add folder project mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,18 +3,21 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style= { styles.container } >
-    <Text>Open up App.js to start working on your app! < /Text>
-      < StatusBar style = "auto" />
-        </View>
+    <View style={styles.container}>
+      <Text style={styles.title}>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#fff',
+  }
 });
