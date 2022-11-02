5a5a7d8dfcce15d75ecb7c805a85e3a84acd5c2f
José Erivam <erivamdev@gmail.com>
2022-11-02 19:31:39 -0300

    :sparkles: feat:add file theme with colors

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,24 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NativeBaseProvider } from 'native-base';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello React Native!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello React Native!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
   title: {
-    color: '#fff',
+    color: 'green',
     fontSize: 24,
   }
 });
