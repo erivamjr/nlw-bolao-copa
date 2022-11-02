606c3af1b57ba4ab77f7ade67ed929214f5f1ab6
José Erivam <erivamdev@gmail.com>
2022-11-02 17:12:10 -0300

    :sparkles: feat:add fetch of api

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,5 +1,20 @@
-export default function Home() {
+interface HomeProps {
+  count: number;
+}
+export default function Home(props: HomeProps) {
   return (
-    <h1>Hello World</h1>
+    <h1>Contagem: {props.count}</h1>
   )
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3001/pools/count')
+  const data = await response.json()
+  console.log(data.count);
+
+  return {
+    props: {
+      count: data.count,
+    }
+  }
 }
