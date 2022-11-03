6974695d50599c7ff855f8c71363c4bb5eb83cae
José Erivam <erivamdev@gmail.com>
2022-11-03 15:15:38 -0300

    :sparkles: feat:add component the signin and loading

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import React from 'react';
 import { NativeBaseProvider, Text, Center } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/SignIn';
+
 import { THEME } from './src/styles/theme';
 
 export default function App() {
-  useFonts({ useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
   return (
-    <NativeBaseProvider>
-      <Center flex={1} bgColor="gray.900">
-        <Text color="white" fontSize={24}>
-          Hello React Native!
-        </Text>
-        <StatusBar style="auto" />
-      </Center>
+    <NativeBaseProvider theme={THEME}>
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
