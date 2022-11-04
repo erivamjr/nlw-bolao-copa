34756fa94bca71f43c6fe74d92382fc5b05d6ea4
José Erivam <erivamdev@gmail.com>
2022-11-04 17:21:50 -0300

    :sparkles: feat:add context, hooks and sharing data

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,18 +6,21 @@ import { Loading } from './src/components/Loading';
 import { SignIn } from './src/screens/SignIn';
 
 import { THEME } from './src/styles/theme';
+import { AuthContextProvider } from './src/context/AuthContext';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle='light-content'
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle='light-content'
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <SignIn /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
---
+++ mobile/src/context/AuthContext.tsx
@@ -0,0 +1,38 @@
+import { createContext, ReactNode } from 'react';
+
+interface UserProps {
+  name: string;
+  avatarUrl: string;
+}
+
+export interface AuthContextDataProps {
+  user: UserProps;
+  signIn: () => Promise<void>;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+
+export const AuthContext = createContext({} as AuthContextDataProps);
+
+export function AuthContextProvider({ children }: AuthProviderProps) {
+
+  async function signIn() {
+    console.log('Vamos Logar!');
+
+  }
+
+  return (
+    <AuthContext.Provider value={{
+      signIn,
+      user: {
+        name: 'Jose Erivam',
+        avatarUrl: 'https://github.com/erivamjr.png'
+      }
+    }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
