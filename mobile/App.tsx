cbccd3a805e521b2d54e6f10c9435c0415840576
José Erivam <erivamdev@gmail.com>
2022-11-06 08:57:32 -0300

    :sparkles: feat:add custom buttom tab

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { AuthContextProvider } from './src/context/AuthContext';
+
+import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
-// import { SignIn } from './src/screens/SignIn';
 
 import { THEME } from './src/styles/theme';
-import { AuthContextProvider } from './src/context/AuthContext';
-// import { New } from './src/screens/New';
-import { Pools } from './src/screens/Pools';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
@@ -21,7 +20,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <Pools /> : <Loading />}
+        {fontsLoaded ? <Routes /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
