c64f5bd03e6f2724f523e36638c88daf898598a5
José Erivam <erivamdev@gmail.com>
2022-11-06 10:11:46 -0300

    :sparkles: feat:add redirect of pages and authorizations

--- mobile/src/context/AuthContext.tsx
+++ mobile/src/context/AuthContext.tsx
@@ -2,10 +2,12 @@ import { createContext, ReactNode, useState, useEffect } from 'react';
 import * as Google from 'expo-auth-session/providers/google';
 import * as AuthSession from 'expo-auth-session';
 import * as WebBrowser from 'expo-web-browser';
+import { api } from '../services/api';
 
 WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
+  user: string;
   name: string;
   avatarUrl: string;
 }
@@ -49,6 +51,21 @@ export function AuthContextProvider({ children }: AuthProviderProps) {
 
   async function singInWithGoogle(access_token: string) {
     console.log("TOKEN DE AUTENTICACAO", access_token);
+    try {
+      setIsUserLoading(true);
+
+      const tokenResponse = await api.post('/users', { access_token })
+      api.defaults.headers.common['Authorization'] = `Bearer ${tokenResponse.data.token}`
+
+      const userInfoResponse = await api.get('/me');
+      setUser(userInfoResponse.data.user)
+
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setIsUserLoading(false);
+    }
   }
 
   useEffect(() => {
